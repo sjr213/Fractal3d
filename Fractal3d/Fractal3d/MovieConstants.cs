436e6e640d6ceb9c77a26e41668631807a009c54
sjr213 <sjrobles@yahoo.com>
2023-10-03 20:03:02 -0400

    Created IntRangeRuleUsingMaxWrapper for CurrentImage validation in MoviewView.

---
+++ Fractal3d/Fractal3d/MovieConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Fractal3d;
+
+public static class MovieConstants
+{
+    public const int FirstImage = 1;
+    public const int MinImages = 2;
+    public const int MaxImages = 10000;
+}
+
