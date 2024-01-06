96fd126353b531b27c9abc199e2abe67f8607c0b
Steve Robles <sjrobles@yahoo.com>
2024-01-05 20:01:33 -0500

    Add MovieUtil.cs

---
+++ Fractal3d/Fractal3d/MovieUtil.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using AForge.Video.FFMPEG;
+
+namespace Fractal3d;
+
+// https://stackoverflow.com/questions/9744026/image-sequence-to-video-stream
+public static class MovieUtil
+{
+    public static void CreateMovie(string filename, int width, int height, int frameRate, List<System.Drawing.Bitmap> images)
+    {
+        using (var vfWriter = new VideoFileWriter())
+        {
+            vfWriter.Open(filename, width, height, frameRate, VideoCodec.Raw);
+            foreach (var image in images)
+            {
+                vfWriter.WriteVideoFrame(image);
+            }
+
+            vfWriter.Close();
+        }
+    }
+}
