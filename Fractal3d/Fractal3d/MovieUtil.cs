30a3eeab6d955c1f96e06a4a4431cdb15aa900dc
Steve Robles <sjrobles@yahoo.com>
2025-03-07 20:40:20 -0500

    Fixed saving mp3 movies repeatably without closing app.

--- Fractal3d/Fractal3d/MovieUtil.cs
+++ Fractal3d/Fractal3d/MovieUtil.cs
@@ -10,24 +10,31 @@ using FFMediaToolkit;
 // https://stackoverflow.com/questions/9744026/image-sequence-to-video-stream
 public static class MovieUtil
 {
+    private const string ffmpegPath = "C:\\ffmpeg\\bin\\";
+
     public static void CreateMovieMp4(string filename, int width, int height, int frameRate, List<Bitmap> images)
     {
-        FFmpegLoader.FFmpegPath = @"C:\ffmpeg\bin\";
-        var settings = new VideoEncoderSettings(width, height, frameRate, codec: VideoCodec.H264)
-            {
-                EncoderPreset = EncoderPreset.Medium,
-                CRF = 17
-            };
-        var file = MediaBuilder.CreateContainer(filename).WithVideo(settings).Create();
-        foreach (var bitmap in images)
-        {
-            var rect = new Rectangle(Point.Empty, bitmap.Size);
-            var bitLock = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-            var bitmapData = ImageData.FromPointer(bitLock.Scan0, ImagePixelFormat.Bgr24, bitmap.Size);
-            file.Video.AddFrame(bitmapData); // Encode the frame
-            bitmap.UnlockBits(bitLock);
-        }
 
-        file.Dispose();
+        if (FFmpegLoader.FFmpegPath != ffmpegPath)
+            FFmpegLoader.FFmpegPath = ffmpegPath;
+
+        var settings = new VideoEncoderSettings(width, height, frameRate, codec: VideoCodec.H264)
+        {
+            EncoderPreset = EncoderPreset.Medium,
+            CRF = 17
+        };
+
+        using (var file = MediaBuilder.CreateContainer(filename).WithVideo(settings).Create())
+        { 
+            foreach (var bitmap in images)
+            {
+                var rect = new Rectangle(Point.Empty, bitmap.Size);
+                var bitLock = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+                var bitmapData = ImageData.FromPointer(bitLock.Scan0, ImagePixelFormat.Bgr24, bitmap.Size);
+                file.Video.AddFrame(bitmapData); // Encode the frame
+                bitmap.UnlockBits(bitLock);
+            }
+
+        }
     }
 }
