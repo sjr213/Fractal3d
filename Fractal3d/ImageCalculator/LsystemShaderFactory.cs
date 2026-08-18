d3f6f18205ad53e2e2de8f16016fb7e903761003
Steve Robles <sjrobles@yahoo.com>
2026-08-18 19:12:29 -0400

    Updated LsystemShaderFactory to use branches (doesn’t use position yet)

--- Fractal3d/ImageCalculator/LsystemShaderFactory.cs
+++ Fractal3d/ImageCalculator/LsystemShaderFactory.cs
@@ -4,6 +4,8 @@ using FractureCommonLib;
 using System.Numerics;
 using System.Reactive.Subjects;
 
+internal record ProcessedBranch(float RelativePosition, float Attenuation, Matrix4x4 RotationMatrix);
+
 public static class MatrixProvider2
 {
     public static Matrix4x4 GetLeftMatrix()
@@ -15,16 +17,17 @@ public static class MatrixProvider2
     {
         return TransformationCalculator.CreateRotationMatrix(0, 0, 30.0f);
     }
-
 }
+
+
 
 public class LsystemShaderFactory : IDisposable
 {
     private FractalParams _fractalParams = new(FractalParams.MakeLights());
     private readonly Subject<double> _progressSubject = new();
     public IObservable<double> Progress => _progressSubject;
-    private Func<Vector3, float> _distanceEstimator = EstimateDistanceSphere;
-
+    private List<ProcessedBranch> _processedBranches = new();
+    private Func<Vector3, float> _distanceEstimator = (Vector3 p) => EstimateDistanceComposite(p, 0, 0.0f, new List<ProcessedBranch>());
     private bool _isDisposed;
 
     public void Dispose()
@@ -47,59 +50,15 @@ public class LsystemShaderFactory : IDisposable
         _isDisposed = true;
     }
 
-    public static float EstimateDistanceSphere(Vector3 p)
+    internal static List<ProcessedBranch> ProcessBranches(List<LSystemBranch> branches)
     {
-        // sphere at origin 0,0,0 of radius 0.4
-        // (p-circleOrigin).Length - radius
-        return p.Length() - 0.4f;
-    }
-
-    public static float EstimateDistanceBox(Vector3 p)
-    {
-        p -= new Vector3(1f, 0.25f, 0.1f);  // this moves the box to the right
-        var box = new Vector3(0.3f, 0.25f, 0.1f);
-        var q = Vector3.Abs(p) - box;
-        return (Vector3.Max(q, Vector3.Zero)).Length() + Math.Min(Math.Max(Math.Max(q.Y, q.Z), q.X), 0.0f);
-    }
-
-    public static Vector3 MaxVec(Vector3 a, float b)
-    {
-        return new Vector3(Math.Max(a.X, b), Math.Max(a.Y, b), Math.Max(a.Z, b));
-    }
-
-    public static float EstimateDistanceBox2(Vector3 p)
-    {
-        var b = new Vector3(0.5f, 0.25f, 0.1f);
-        Vector3 q = Vector3.Abs(p) - b;
-        var l = MaxVec(q, 0.0f).Length();
-        return (l + (float)Math.Min(Math.Max(q.X, Math.Max(q.Y, q.Z)), 0.0));
-    }
-
-    public static float EstimateDistanceTorus(Vector3 p)
-    {
-        Vector2 t = new Vector2(0.25f, 0.05f);
-        Vector2 pxz = new Vector2(p.X, p.Z);
-        Vector2 q = new Vector2(pxz.Length() - t.X, p.Y);
-        return q.Length() - t.Y;
-    }
-
-    public static float EstimateDistanceCapsule(Vector3 p)
-    {
-        Vector3 a = new Vector3(-0.25f, 0.25f, 0.1f);
-        Vector3 b = new Vector3(0.25f, -0.25f, -0.1f);
-        float r = 0.1f;
-        return sdCapsule(p, a, b, r);
-    }
-
-    public static Vector3 RotateEnd(Vector3 start, Vector3 end, float angle)
-    {
-        var cos = (float)Math.Cos(angle * Math.PI / 180.0);
-        var sin = (float)Math.Sin(angle * Math.PI / 180.0);
-
-        var x = start.X + (end.X - start.X) * cos - (end.Y - start.Y) * sin;
-        var y = start.Y + (end.X - start.X) * sin + (end.Y - start.Y) * cos;
-
-        return new Vector3(x, y, end.Z);
+        var processedBranches = new List<ProcessedBranch>();
+        foreach (var branch in branches)
+        {
+            var rotationMatrix = TransformationCalculator.CreateRotationMatrix(branch.AngleXDegrees, branch.AngleYDegrees, branch.AngleZDegrees);
+            processedBranches.Add(new ProcessedBranch(branch.RelativePostionX, branch.Attenuation, rotationMatrix));
+        }
+        return processedBranches;
     }
 
     public static Vector3 RotateEnd2(Vector3 start, Vector3 end, Matrix4x4 mat)
@@ -109,45 +68,7 @@ public class LsystemShaderFactory : IDisposable
         return inter + start;
     }
 
-    record struct PointPair(Vector3 Start, Vector3 End);
-
-    public static float EstimateDistanceCompositeOld(Vector3 p)
-    {
-        var start = new Vector3(0.0f, 0.9f, 0.0f);
-        var end = new Vector3(0.0f, 0.2f, 0.0f);
-        float r = 0.1f;
-
-        var pts = new List<PointPair>
-        {
-            new PointPair(start, end)
-        };
-        var height = (start - end).Length();
-        float distance = float.MaxValue;
-        var leftMatrix = MatrixProvider.GetLeftMatrix();
-        var rightMatrix = MatrixProvider.GetRightMatrix();
-
-        for (int i = 0; i < 4; i++)
-        {
-            height = height *= 0.7f;
-            var pts2 = new List<PointPair>();
-
-            foreach (var pt in pts.ToList())
-            {
-                distance = Math.Min(distance, sdCapsule(p, pt.Start, pt.End, r));
-                var start2 = pt.End;
-                var end2 = pt.End;
-                end2.Y -= height;
-                var left = RotateEnd2(start2, end2, leftMatrix);
-                var right = RotateEnd2(start2, end2, rightMatrix);
-
-                pts2.Add(new PointPair(start2, left));
-                pts2.Add(new PointPair(start2, right));
-            }
-            height *= 0.7f;
-            pts = pts2;
-        }
-        return distance;
-    }
+    record struct PointPair(Vector3 Start, Vector3 End, float Length);
 
     private static Vector3 GetNewEnd(Vector3 start, Vector3 end, Matrix4x4 mat, float relativeLength)
     {
@@ -156,36 +77,34 @@ public class LsystemShaderFactory : IDisposable
         return RotateEnd2(end, newEnd, mat);
     }
 
-    public static float EstimateDistanceComposite(Vector3 p)
+    internal static float EstimateDistanceComposite(Vector3 p, int iterations, float radius, List<ProcessedBranch> processedBranches)
     {
         var start = new Vector3(0.0f, 0.9f, 0.0f);
         var end = new Vector3(0.0f, 0.2f, 0.0f);
-        float r = 0.1f;
-        float attenuation = 1.05f;
 
+        var length = (end - start).Length();
         var pts = new List<PointPair>
         {
-            new PointPair(start, end)
+            new PointPair(start, end, length)
         };
-        var length = (end - start).Length();
+        
         float distance = float.MaxValue;
-        var leftMatrix = MatrixProvider.GetLeftMatrix();
-        var rightMatrix = MatrixProvider.GetRightMatrix();
 
-        for (int i = 0; i < 4; i++)
+        // NEED TO FIX LENGTH AND ATTENUATION
+        for (int i = 0; i < iterations; i++)
         {
-            length = length *= attenuation;
             var pts2 = new List<PointPair>();
 
             foreach (var pt in pts.ToList())
             {
-                distance = Math.Min(distance, sdCapsule(p, pt.Start, pt.End, r));
+                distance = Math.Min(distance, sdCapsule(p, pt.Start, pt.End, radius));
 
-                var left = GetNewEnd(pt.Start, pt.End, leftMatrix, length);
-                var right = GetNewEnd(pt.Start, pt.End, rightMatrix, length);
-
-                pts2.Add(new PointPair(pt.End, left));
-                pts2.Add(new PointPair(pt.End, right));
+                foreach(var branch in processedBranches)
+                {
+                    var newLength = pt.Length * branch.Attenuation;
+                    var newEnd = GetNewEnd(pt.Start, pt.End, branch.RotationMatrix, newLength);
+                    pts2.Add(new PointPair(pt.End, newEnd, newLength));
+                }
             }
             pts = pts2;
         }
@@ -238,7 +157,6 @@ public class LsystemShaderFactory : IDisposable
         var size = _fractalParams.ImageSize;
         var palette = _fractalParams.Palette;
 
-
         float left = Math.Min(_fractalParams.FromX, _fractalParams.ToX);
         float right = Math.Max(_fractalParams.FromX, _fractalParams.ToX);
         float bottom = Math.Min(_fractalParams.FromY, _fractalParams.ToY);
@@ -255,6 +173,7 @@ public class LsystemShaderFactory : IDisposable
         var transformMatrix = TransformationCalculator.CreateInvertedTransformationMatrix(_fractalParams.TransformParams);
         var transformedLights = LightUtil.TransformLights(_fractalParams.Lights, transformMatrix);
         var transViewPos = TransformationCalculator.Transform(transformMatrix, viewPos);
+        _distanceEstimator = (Vector3 p) => EstimateDistanceComposite(p, _fractalParams.Iterations, _fractalParams.LSystemRadius, _processedBranches);
 
         for (var x = 0; x < size.Width; ++x)
         {
@@ -274,6 +193,7 @@ public class LsystemShaderFactory : IDisposable
                 var hit = RayMarch(startPt, direction, transformMatrix, out var outPt);
 
                 var transformedPt = TransformationCalculator.Transform(transformMatrix, outPt);
+
                 var normal = NormalCalculator.CalculateNormal(_distanceEstimator, _fractalParams.NormalDistance, transformedPt);
 
                 var lighting = (hit) ? LightUtil.GetPointLight(transformedLights, _fractalParams.LightComboMode, transformedPt, transViewPos, normal) :
@@ -296,25 +216,6 @@ public class LsystemShaderFactory : IDisposable
         _progressSubject.OnNext(startProgress + sumProgress);
     }
 
-    private static Func<Vector3, float> GetSceneDel(ShaderSceneType sceneType)
-    {
-        switch (sceneType)
-        {
-            case ShaderSceneType.Sphere:
-                return EstimateDistanceSphere;
-            case ShaderSceneType.Box:
-                return EstimateDistanceBox2;
-            case ShaderSceneType.Torus:
-                return EstimateDistanceTorus;
-            case ShaderSceneType.Capsule:
-                return EstimateDistanceCapsule;
-            case ShaderSceneType.Composite:
-                return EstimateDistanceComposite;
-            default:
-                throw new ArgumentException("Unknown Scene Type");
-        }
-    }
-
     public async Task<FractalResult> CreateShaderAsync(FractalParams fractalParams, double startProgress, double sumProgress, CancellationToken cancelToken)
     {
         _fractalParams = fractalParams;
@@ -322,10 +223,10 @@ public class LsystemShaderFactory : IDisposable
         var raw = new RawLightedImage(size.Width, size.Height, fractalParams.Palette.NumberOfColors);
         raw.LightingOnZeroIndex = fractalParams.LightingOnZeroIndex;
 
-        _distanceEstimator = GetSceneDel(fractalParams.SceneType);
-
         if (cancelToken.IsCancellationRequested)
             return new FractalResult();
+
+        _processedBranches = ProcessBranches(fractalParams.LSystemBranches);
 
         await Task.Run(() => CalculateImageNew(raw, startProgress, sumProgress, cancelToken), cancelToken);
 
